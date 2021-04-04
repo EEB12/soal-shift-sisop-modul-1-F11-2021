b52436bbcd414c74009267bfd386e52856f794a7
juliettaanastasia <74484044+juliettaanastasia@users.noreply.github.com>
2021-04-04 21:24:02 +0700

    Update soal1.sh

--- Soal 1/soal1.sh
+++ Soal 1/soal1.sh
@@ -16,11 +16,27 @@ echo "$(grep -oE 'ERROR.*' syslog.log)" | grep -oE "([A-Z][a-z]+)\s(['A-Za-z]+\s
 printf "==========================\n";
 printf "Total semua kemunculan error : %d\n" $(grep -c "ERROR" syslog.log); # Menghitung jumlah kata ERROR
 
+# Soal 1 c
+username=`cut -d"(" -f2 < syslog.log | cut -d")" -f1 | sort | uniq`
+user_error=$(grep -cP "ERROR.*($name)" syslog.log);
+user_info=$(grep -cP "INFO.*($name)" syslog.log);
+for name in $username
+do
+    printf "%s, Error : %d, Info : %d\n" $name "$user_error" "$user_info"
+done
+
 # Soal 1 d
-printf "ERROR,COUNT\n" > "error_message.csv"; #header
+printf "ERROR,COUNT\n" > "error_message.csv"; # header
 echo "$(grep -oE 'ERROR.*' syslog.log)" | grep -oE "([A-Z][a-z]+)\s(['A-Za-z]+\s){1,6}" | sort | uniq |
     while read -r row
     do
         countError=$(grep -c "$row" syslog.log);
         echo "$row,$countError";
     done | sort -rt',' -nk2 >> error_message.csv;
+
+# Soal 1 e
+printf "Username,INFO,ERROR\n" > "user_statistic.csv"; #header
+for name in $username
+do
+    printf "%s,%d,%d\n" $name $(grep -cP "INFO.*($name)" syslog.log) $(grep -cP "ERROR.*($name)" syslog.log);
+done | sort >> user_statistic.csv;
