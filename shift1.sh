3c771629db08c205d79c1d5d1c0217075459938f
juliettaanastasia <74484044+juliettaanastasia@users.noreply.github.com>
2021-04-04 15:42:39 +0700

    Update shift1.sh

--- shift1.sh
+++ shift1.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-IFS=
 
-# 1a
-# regex="(INFO |ERROR )(.*)((?=[\(])(.*))"
-regex1="(?<=ERROR )(.*)(?=\ )"
-regex2="(?<=[(])(.*)(?=[)])"
-regex3="(?=[(])(.*)(?<=[)])"
-input="syslog.log";
-# str="Jan 31 00:16:25 ubuntu.local ticky: INFO Closed ticket [#1754] (noel)"
+# Soal 1 a
+grep -oE "(INFO.*)|(ERROR.*)" syslog.log > syslog.txt;
+
+# Soal 1 b
+printf "Error line :\n";
+grep -oE "ERROR\s([A-Z])([a-z]+)(\s[a-zA-Z']+){1,6}" syslog.log;
+printf "==========================\n";
+echo "$(grep -oE 'ERROR.*' syslog.log)" | grep -oE "([A-Z][a-z]+)\s(['A-Za-z]+\s){1,6}" | sort | uniq |
+    while read -r row
+    do
+        countError=$(grep -c "$row" syslog.log);
+        echo "$row,$countError";
+    done | sort -rt',' -nk2;
+printf "==========================\n";
+printf "Total semua kemunculan error : %d\n" $(grep -c "ERROR" syslog.log); # Menghitung jumlah kata ERROR
+
+# Soal 1 d
+printf "ERROR,COUNT\n" > "error_message.csv"; #header
+echo "$(grep -oE 'ERROR.*' syslog.log)" | grep -oE "([A-Z][a-z]+)\s(['A-Za-z]+\s){1,6}" | sort | uniq |
+    while read -r row
+    do
+        countError=$(grep -c "$row" syslog.log);
+        echo "$row,$countError";
+    done | sort -rt',' -nk2 >> error_message.csv;
