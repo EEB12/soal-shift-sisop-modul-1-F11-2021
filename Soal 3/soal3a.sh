043401687b9613cde9e8fb0e8fa395faa2833371
Nazhwa Ameera <68385532+nazhwaameera@users.noreply.github.com>
2021-04-04 14:40:46 +0700

    Revisi

--- Soal 3/soal3a.sh
+++ Soal 3/soal3a.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
+hitung=1
 for((i=1; i<=23; i++))
 do
-	wget -O "Koleksi_$i.jpg" -a Foto.log https://loremflickr.com/320/240/kitten
-done
-
-for((i=1; i<=23; i++))
-do
-	for((j=1; j<i; j++))
-	do
-		if comm Koleksi_$j.jpg Koleksi_$i.jpg &> /dev/null
-		then
-			rm Koleksi_$i.jpg
-			break
-		fi
-	done
-done
-
-for((i=1; i<=23; i++))
-do
-	if [ ! -f Koleksi_$i.jpg ] 
+	wget -o a.log https://loremflickr.com/320/240/kitten
+	if [[ $hitung -gt 1 ]]
 	then
-		for((j=23; j>i; j--))
-		do
-			if [ -f Koleksi_$j.jpg ];
-			then
-				mv Koleksi_$j.jpg Koleksi_$i.jpg
-				break
-			fi
-		done
+		location=`awk '/Location/{print $2}' a.log`
+		compare=`awk -v "loc=$location" '$0 ~ loc {print 1}' Foto.log`
+		if [[ ${#compare} -gt 1 ]]
+		then
+			rm kitten
+			let hitung=$hitung-1
+		else
+			mv kitten Koleksi_$hitung.jpg
+			cat a.log >> Foto.log
+		fi
+		rm a.log
+	else
+		mv kitten Koleksi_$hitung.jpg
+		mv a.log Foto.log
 	fi
+	let hitung=$hitung+1
+
 done
 
 for((i=1; i<=9; i++))
--- Soal 3/soal3c.sh
+++ Soal 3/soal3c.sh
@@ -1,30 +1,97 @@
 #!/bin/bash
 
-directory=$(date +"%d-%m-%Y")
+Download1 () {
+	hitung=1
+	for((i=1; i<=23; i++))
+	do
+        	wget -o a.log https://loremflickr.com/320/240/kitten
+        	if [[ $hitung -gt 1 ]]
+        	then
+                	location=`awk '/Location/{print $2}' a.log`
+                	compare=`awk -v "loc=$location" '$0 ~ loc {print 1}' Foto.log`
+                	if [[ ${#compare} -gt 1 ]]
+                	then
+                        	rm kitten
+                        	let hitung=$hitung-1
+                	else
+                        	mv kitten Koleksi_$hitung.jpg
+                        	cat a.log >> Foto.log
+               	 	fi
+                	rm a.log
+        	else
+                	mv kitten Koleksi_$hitung.jpg
+                	mv a.log Foto.log
+        	fi
+        	let hitung=$hitung+1
+	done
 
-mkdir Kucing_$directory
-mkdir Kelinci_$directory
+	for((i=1; i<=9; i++))
+	do
+        	if [ -f Koleksi_$i.jpg ]
+        	then
+                	mv Koleksi_$i.jpg Koleksi_0$i.jpg
+        	fi
+	done
 
-hitung=1
-for((i=1; i<=31; i++))
-do
-	if [ $((i%2)) -eq 0 ]
+	dirName="Kucing_$(date +%d%m%Y)"
+	if [[ ! -d "$path/$dirName" ]]
 	then
-		wget -O "Kucing_$hitung.jpg" -a Foto.log https://loremflickr.com/320/240/kitten
-	elif [ $((i%2)) -eq 1 ]
-	then
-		wget -O "Kelinci_$hitung.jpg" -a Foto.log https://loremflickr.com/320/240/bunny
+		mkdir $dirName
 	fi
-	let hitung=$hitung+1
-done
+	mv Koleksi_* $dirName
+	mv Foto.log $dirName
+}
 
-for((i=1; i<=31; i++))
-do
-	if [ $((i%2)) -eq 0 ]
-	then
-		mv Kucing_$i.jpg Kucing_$directory
-	elif [ $((i%2)) -eq 1 ]
-	then
-		mv Kelinci_$i.jpg Kelinci_$directory
-	fi
-done
+Download2 () {
+        hitung=1
+        for((i=1; i<=23; i++))
+        do
+                wget -o a.log https://loremflickr.com/320/240/bunny
+                if [[ $hitung -gt 1 ]]
+                then
+                        location=`awk '/Location/{print $2}' a.log`
+                        compare=`awk -v "loc=$location" '$0 ~ loc {print 1}' Foto.log`
+                        if [[ ${#compare} -gt 1 ]]
+                        then
+                                rm kitten
+                                let hitung=$hitung-1
+                        else
+                                mv kitten Koleksi_$hitung.jpg
+                                cat a.log >> Foto.log
+                        fi
+                        rm a.log
+                else
+                        mv bunny Koleksi_$hitung.jpg
+                        mv a.log Foto.log
+                fi
+                let hitung=$hitung+1
+        done
+
+        for((i=1; i<=9; i++))
+	do
+                if [ -f Koleksi_$i.jpg ]
+                then
+                        mv Koleksi_$i.jpg Koleksi_0$i.jpg
+                fi
+        done
+        dirName="Kelinci_$(date +%d%m%Y)"
+        if [[ ! -d "$path/$dirName" ]]
+        then
+                mkdir $dirName
+        fi
+        mv Koleksi_* $dirName
+        mv Foto.log $dirName
+}
+
+path=`pwd`
+previously=$(date -d '-2 day' +%d%m%Y)
+
+if [ -d "$path/Kucing_$previously" ]
+then
+	Download1
+elif [ -d "$path/Kelinci_$previously" ]
+then
+	Download2
+else
+	Download1
+fi
